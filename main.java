766b3ff75c3c80478d5cf7cbc0f4d8613c4f1367
krisez <740879437@qq.com>
2016-09-28 11:10:27 +0800

    demo

--- main.java
+++ main.java
@@ -4,8 +4,12 @@
 import java.util.Calendar;
 class main{
 	public static void main(String args[]){
-	  Calendar c = Calendar.getInstance();
-	  c
+	   Calendar c = Calendar.getInstance();
+        c.getTime();
+        Date date = c.getTime();
+        System.out.println(c.getTimeInMillis());
+        c.set(Calendar.YEAR,Calendar.SEPTEMBER+1,Calendar.DATE,Calendar.HOUR_OF_DAY, Calendar.MINUTE);
+        System.out.println(c.getTimeInMillis());
 	  
 	}
 }
