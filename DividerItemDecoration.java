227f232493394c7137c4fba99f17e532543678db
krisez <740879437@qq.com>
2016-10-28 21:39:03 +0800

    recyclerView"

--- DividerItemDecoration.java
+++ DividerItemDecoration.java
@@ -30,6 +30,7 @@ import android.view.View;
  * This class is from the v7 samples of the Android SDK. It's not by me!
  * <p/>
  * See the license above for details.
+ * RecyclerView about ItemDecoration
  * 
  */
 public class DividerItemDecoration extends RecyclerView.ItemDecoration
