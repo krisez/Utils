16bc78c5821a4aed57eeffdb497f2d74d4c39cd0
Kkkkkk <740879437@qq.com>
2019-03-06 09:36:17 +0800

    只保留数据转换

--- Utils.java
+++ Utils.java
@@ -1,55 +1,11 @@
 import android.content.Context;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Created by Krisez on 2016/9/13.
  */
 public class Utils {
-    public static void setHttpConnection(final String address, final MyCallBack myCallback){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                HttpURLConnection httpURLConnection =null;
-                try {
-                    URL url = new URL(address);
-                    httpURLConnection = (HttpURLConnection) url.openConnection();
-                    httpURLConnection.setRequestMethod("GET");
-                    httpURLConnection.setConnectTimeout(1000);
-                    httpURLConnection.setReadTimeout(1000);
-                    InputStream in = httpURLConnection.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null){
-                        response.append(line);
-                    }
-                    if(myCallback != null){
-                        myCallback.onFinish(response.toString());
-                    }
-                } catch (Exception e) {
-                    if(myCallback != null){
-                        myCallback.onError(e);
-                    }
-                }
-                finally {
-                    if(httpURLConnection != null){
-                        httpURLConnection.disconnect();
-                    }
-                }
-            }
-        }).start();
-    }
-
-    protected interface MyCallBack{
-        void onFinish(String response);
-        void onError(Exception e);
-    }
 	//父类的宽度
-	public static int getParentWidth(Context context){
+    public static int getParentWidth(Context context){
         WindowManager wm=(WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics outMetrics = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(outMetrics);
