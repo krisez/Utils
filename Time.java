761c3351979c568561f2ded32f7afb9f913d15f4
Kkkkkk <740879437@qq.com>
2019-03-06 09:34:42 +0800

    修改得到两天相差时间

--- Time.java
+++ Time.java
@@ -44,56 +44,14 @@ public class Time {
 
     /**
      *
-     * 与当天相差毫秒数
+     * 与某天相差毫秒数
      * @return
      */
-    public static int getDate(){
-        int monthHave[] = {31,0,31,30,31,30,31,31,30,31,30,31};
-		//初始化时间。。。i think should start at net time
-        int initYear = 2016;
-        int initMonth = 9;
-        int initDay = 5;
-		//~~~~~~~~
-		//theTime is init day
-        int theTime = 0;
-        Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        month = month + 1;
-        int day = c.get(Calendar.DAY_OF_MONTH);
-
-        if((year % 4 == 0 && year % 100 !=0) || year % 400 == 0){
-            allYearDay = 366;
-            monthHave[1] = 29;
-        }
-        else{
-            allYearDay = 365;
-            monthHave[1] = 28;
-        }
-
-        if(year == initYear){
-            if(month == initMonth){
-                if(day > initDay){
-                    theTime = day-initDay;
-                }
-            }else {
-                switch (month){
-                    case 10:theTime = 30 - initDay + day;
-                        break;
-                    case 11:theTime = 30 - initDay + 31 + day;
-                        break;
-                    case 12:theTime = 30 - initDay + day + 31 + 30;
-                        break;
-                }
-            }
-        }
-        else {
-            theTime = 30 - initDay + 31 + 30 + 31;
-            for(int i = 1;i < month;i++){
-                theTime += monthHave[i];
-            }
-            theTime += day;
-        }
-        return theTime;
+    public static long getMill(int y,int m,int d){
+	Calendar c = Calendar.getInstance();
+	long now = c.getTimeInMillis();
+	c.set(y,m-1,d);
+	long u = c.getTimeInMillis();
+	return u-now;
     }
 }
